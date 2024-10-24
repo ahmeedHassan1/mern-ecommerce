38d3f8d85061dd9cc7e390cab96b17900bb8d971
Ahmed Hassan <ahmedff35@gmail.com>
2024-10-24 05:35:39 +0300

    Finale

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -6,7 +6,7 @@ import Product from "../models/product.js";
 // @route   GET /api/products
 // @access  Public
 const getProducts = asyncHandler(async (req, res) => {
-	const pageSize = 4;
+	const pageSize = process.env.PAGINATION_LIMIT || 10;
 	const page = Number(req.query.pageNumber) || 1;
 
 	const keyword = req.query.keyword
