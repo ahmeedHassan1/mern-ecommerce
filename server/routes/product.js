37d6c1c48f74a932520c1a5db3662daee0529e18
Ahmed Hassan <ahmedff35@gmail.com>
2024-10-24 04:26:40 +0300

    Review Product

--- server/routes/product.js
+++ server/routes/product.js
@@ -6,7 +6,8 @@ import {
 	getProducts,
 	createProduct,
 	updateProduct,
-	deleteProduct
+	deleteProduct,
+	createProductReview
 } from "../controllers/product.js";
 import { protect, admin } from "../middleware/auth.js";
 
@@ -16,5 +17,6 @@ router
 	.get(getProductById)
 	.put(protect, admin, updateProduct)
 	.delete(protect, admin, deleteProduct);
+router.route("/:id/reviews").post(protect, createProductReview);
 
 export default router;
